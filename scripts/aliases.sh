4ebbab4b3fd2640a26efafe9b43ab38b8c1eb119
Yin-Chi Chan <ycc39@cam.ac.uk>
2025-12-03 13:15:40 +0000

    Add eza to Docker host, update aliases

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -3,3 +3,9 @@
 
 git config alias.root 'rev-parse --show-toplevel'
 git config alias.st 'status -sb -uall --ahead-behind'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+alias e='eza -lhog -F --group-directories-first --no-permissions --smart-group'
